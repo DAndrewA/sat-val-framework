923e560c6e1b9cca8c3f1e094ed2df4af1688571
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-08-22 19:21:29 +0100

    #12 remove CollocatedHomogenisedDataList import from __init__

---
+++ sat-val-framework/__init__.py
@@ -0,0 +1,5 @@
+from .usable import (
+    CollocationEventList,
+    CollocatedRawDataList,
+    #CollocatedHomogenisedDataList # no available import as should be created through a CollocatedRawDataList instance
+)
