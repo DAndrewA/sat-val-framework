e3a34a439d6da6879c21c1bbad8ef2e40cd89050
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-08-28 20:47:32 +0100

    implement Duration for raw_cloudnet, and loading from event logic

--- example/definitions/raw_cloudnet.py
+++ example/definitions/raw_cloudnet.py
@@ -6,19 +6,25 @@ Class definition for RawCloudnet to handle raw data from .nc files.
 
 from __future__ import annotations
 
-#from collocation import RadiusDuration
-#TODO: remove cyclic dependency between RadiusDuration and RawCloudnet
-class RadiusDuration: pass # defined for type checking reasons
-from sat_val_framework.implement import RawData
+from sat_val_framework.implement import (
+    RawData,
+    RawMetadata,
+    RawDataSubsetter,
+    CollocationEvent,
+)
 
+import cloudnet_decode
+
+from dataclasses import dataclass
+from pandas import Timestamp
 import xarray as xr 
+import numpy as np
 import os
 import datetime as dt
 
 
 _Cloudnet_required_vars = (
     "longitude", "latitude",
-    "classification_bits", "quality_bits",
 )
 
 
@@ -37,8 +43,8 @@ def _dates_from_time_slice(tslice: slice) -> list[dt.date]:
 def _load_cloudnet_from_file(fpath: str) -> xr.Dataset:
     """Function that, given a fully qualified file path to Cloudnet data, loads it and returns an xarray dataset"""
     ds = xr.open_dataset(fpath)
-    # TODO: add quality and category bits to the dataset
-    # TODO: ds = _include_cloudnet_cloudmask(ds)
+    ds = cloudnet_decode.decode_cat_ds(ds)
+    ds = _include_cloudnet_cloudmask(ds)
     return ds
 
 
@@ -51,6 +57,79 @@ def _include_cloudnet_cloudmask(ds: xr.Dataset) -> xr.Dataset:
         data.liq | (data.falling & data.cold)
     ).rename("cloudmask")
     return data
+
+
+
+@dataclass(kw_only=True, frozen=True)
+class Duration(RawDataSubsetter):
+    duration: dt.timedelta
+
+    def subset(self, raw_data: RawCloudnet) -> RawCloudnet:
+        new_data = raw_data.data.copy()
+        new_metadata = raw_data.metadata
+        # if no closest approach time is known, cannot subset by window centered on it
+        if not isinstance(new_metadata.loader, CloudnetEvent):
+            print(new_metadata, self, f"No closest approach time known, so no subsetting")
+            return raw_data
+
+        new_metadata.subsetter.append(self)
+
+        center = new_metadata.loader.closest_approach_time
+        tslice = slice(
+            center - 0.5*self.duration,
+            center + 0.5*self.duration
+        )
+        # see if more data needs loading or if current spans sufficient time
+        min_time = Timestamp(new_data.time.min().values).to_pydatetime()
+        max_time = Timestamp(new_data.time.max().values).to_pydatetime()
+        tslice_in_time_range = (
+            (tslice.start >= min_time) &
+            (tslice.stop <= max_time)
+        )
+        if tslice_in_time_range:
+            new_data = new_data.sel(time=tslice)
+            return RawCloudnet(
+                data = new_data,
+                metadata = new_metadata
+            )
+        # if full time slice not already contained, load more data!
+        dates_to_load = _dates_from_time_slice(tslice)
+        fpaths = [
+            os.path.join(
+                new_metadata.loader.root_dir,
+                RawCloudnet._fname_from_datetime(
+                    datetime = date,
+                    site = new_metadata.loader.site
+                )
+            )
+            for date in dates_to_load
+        ]
+        new_data = xr.concat(
+            [
+                _load_cloudnet_from_file(fpath)
+                for fpath in fpaths
+            ],
+            dim="time"
+        ).sel(time=tslice)
+        return RawCloudnet(
+            data = new_data,
+            metadata = new_metadata
+        )
+
+
+
+@dataclass(frozen=True, kw_only=True)
+class CloudnetEvent(CollocationEvent):
+    """Class handling minimum required information to load file containing a collocation event.
+
+    ATTRIBUTES:
+        closest_approach_time (dt.datetime): datetime object describing the time of closest approach of ICESat-2 to the Cloudnet site. Used to center the time window that is loaded.
+        root_dir (str): Path of the directory containing the .nc Cloudnet files to be loaded.
+        site (str): Name of the site for which Cloudnet data is loaded. This is used in generating filenames to be loaded.
+    """
+    closest_approach_time: dt.datetime
+    root_dir: str
+    site: str
 
 
 
@@ -68,51 +147,39 @@ class RawCloudnet(RawData):
 
 
     @classmethod
-    def from_collocation_event_and_parameters(cls, event: CollocationEvent, parameters: CollocationParameters | None) -> Self:
-        assert isinstance(parameters, None | RadiusDuration), f"{type(parameters)=} subsetting is not implemented for {cls}"
-        cne = event.cloudnet_event
-        t_0 = cne.closest_approach_time
-        site = cne.site
-        root_dir = cne.root_dir 
+    def from_collocation_event_and_parameters(cls, event: CloudnetEvent, parameters: Duration | None) -> Self:
+        assert isinstance(event, CloudnetEvent), f"{type(event)=} must be CloudnetEvent"
+        #assert isinstance(parameters, None | RadiusDuration), f"{type(parameters)=} subsetting is not implemented for {cls}"
+        metadata = RawMetadata(
+            loader = event,
+            subsetter = list()
+        )
 
-        # initial implementation is for parameters is None
+        t_0 = event.closest_approach_time
+        site = event.site
+        root_dir = event.root_dir 
+
+        # initially load single day of data, then apply subsetter if present
+        fpath = os.path.join(
+            root_dir,
+            cls._fname_from_datetime(
+                datetime=t_0, 
+                site=site
+            )
+        )
+        data = _load_cloudnet_from_file(fpath = fpath)
+        raw_data = cls(
+            data = data,
+            metadata = metadata
+        )
         if parameters is None:
-            fpath = os.path.join(
-                root_dir,
-                cls._fname_from_datetime(
-                    datetime=t_0, 
-                    site=site
-                )
-            )
-            data = _load_cloudnet_from_file(fpath = fpath)
+            return raw_data
+        else:
+            return parameters.subset(raw_data)
 
-        elif isinstance(parameters, RadiusDuration):
-            tau = parameters.duration
-            time_slice = slice(t_0 - 0.5*tau, t_0 + 0.5*tau)
-            fpaths = [
-                os.path.join(
-                    root_dir,
-                    fname
-                )
-                for fname in [
-                    cls._fname_from_datetime(
-                        datetime = date,
-                        site = site
-                    )
-                    for date in _dates_from_time_slice(time_slice)
-                ]
-            ]
-            data = xr.merge(
-                [
-                    cls.from_qualified_file(fpath)
-                    for fpath in fpaths
-                ],
-                #TODO: assess if this needs special handling to avoid errors with model_time, etc
-            )
-
-        return cls(data=data, metadata=event)
-         
-
+    def perform_qc(self) -> Self:
+        #TODO: implement
+        return self
 
     def homogenise_to(self, H: Type[HomogenisedData]) -> H:
         raise NotImplementedError(f"Type {type(self)} does not implement .homngenise_to(self, H: Type[HomogenisedData])")
@@ -124,7 +191,7 @@ class RawCloudnet(RawData):
         NOTE: this is not a fully qualified filepath. Prepend the directory for this to be the case.
         """
         # TODO: ensure that handling of "summit" is correct
-        fname = f"{datetime:%Y%m%d}_{site}_classfification.nc"
+        fname = f"{datetime:%Y%m%d}_{site}_categorize.nc"
         if site == "summit":
-            fname = "{datetime:%Y%m%d}_classfification.nc"
+            fname = "{datetime:%Y%m%d}_categorize.nc"
         return fname
