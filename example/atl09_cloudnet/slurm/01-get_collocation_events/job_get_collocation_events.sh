35f7aad36ad3436aff92fc7c3485e8de2b1799e6
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-09-15 12:18:08 +0100

    01: increase search radius to 510 km

--- example/atl09_cloudnet/slurm/01-get_collocation_events/job_get_collocation_events.sh
+++ example/atl09_cloudnet/slurm/01-get_collocation_events/job_get_collocation_events.sh
@@ -11,4 +11,4 @@ mamba activate overpass_analysis_again
 
 echo $site $SLURM_ARRAY_TASK_ID
 
-(cd $run_from_dir && time python -m $script_name --dir-atl09 $sites_dir/$site/atl09 --dir-cloudnet $sites_dir/$site/cloudnet --site $site --output-dir $output_dir --job-array-index=$SLURM_ARRAY_TASK_ID)
+(cd $run_from_dir && time python -m $script_name --dir-atl09 $sites_dir/$site/atl09 --dir-cloudnet $sites_dir/$site/cloudnet --site $site --output-dir $output_dir --job-array-index=$SLURM_ARRAY_TASK_ID --R-min-km 510)
