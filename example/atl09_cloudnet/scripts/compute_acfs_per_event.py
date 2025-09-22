6597c9287c9207ccaa99837b8a01a061cd92a2fe
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-09-22 16:30:48 +0100

    compute_acfs_per_event implementation based on compute_vcfs_per_event

---
+++ example/atl09_cloudnet/scripts/compute_acfs_per_event.py
@@ -0,0 +1,337 @@
+"""Author: Andrew Martin
+Creation date: 22/9/2025
+
+Script to handle computing the areal cloud fraction for all collocation events between ICESat-2 and Cloudnet for a given collocaiton event
+"""
+
+
+from ..definitions.collocation import (
+    DistanceFromLocation, Duration, RadiusDuration,
+    RawATL09, RawCloudnet
+)
+from ..definitions import (
+    acf,
+    indices
+)
+from sat_val_framework import CollocationEventList
+
+from typing import Callable, Any
+from dataclasses import dataclass
+import datetime as dt
+import os
+import xarray as xr
+
+
+
+SITES = {
+    "ny-alesund": dict(lat=78.923, lon=11.922), 
+    "hyytiala": dict(lat=61.844, lon=24.287), 
+    "juelich": dict(lat=50.908, lon=6.413), 
+    "munich": dict(lat=48.148, lon=11.573),
+}
+
+
+
+@dataclass(kw_only=True, frozen=True)
+class Args:
+    fpath_pickle: str
+    site: str
+    batched: bool
+    batch: slice
+    dir_output: str
+    joint_params: RadiusDuration
+
+
+def parse_args() -> None | Args:
+    import argparse
+    parser = argparse.ArgumentParser(description="Compute ACFs for ATL09 and CloudNet data")
+
+    # Required arguments (no default values in dataclass)
+    parser.add_argument(
+        '--pickle-dir',
+        type=str,
+        required=True,
+        help='Directory path for the pickled CollocationEventList files'
+    )
+
+    parser.add_argument(
+        '--site',
+        type=str,
+        choices = SITES.keys(),
+        required=True,
+        help='Site identifier'
+    )
+
+    parser.add_argument(
+        '--output-dir',
+        type=str,
+        required=True,
+        help='Directory path to location for saving output netcdf files'
+    )
+
+    parser.add_argument(
+        '--job-array-index',
+        type=int,
+        required=True,
+        help='Job array index'
+    )
+
+    parser.add_argument(
+        "--index-function",
+        choices = indices.INDEX_FUNCTIONS.keys(),
+        required=True,
+        help="Name of the function mapping job array indices to (R,tau) pairs"
+    )
+
+    ### BATCHING LOGIC
+    parser.add_argument(
+        "--batch",
+        action = "store_true",
+        help="If supplied, collocation events are handled in batches",
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        help="Size of collocation event batches. MUST be supplied if --batch given"
+    )
+
+
+    parsed_args = parser.parse_args()
+    
+    site = parsed_args.site
+
+    # get R, tau from array index
+    index_function = indices.INDEX_FUNCTIONS[parsed_args.index_function]
+
+    # default arguments if not batching
+    index = parsed_args.job_array_index
+    batch = slice(None, None, None)
+
+    if (batched:=parsed_args.batch):
+        if not (batch_size := parsed_args.batch_size):
+            raise argparse.ArgumentError("When --batch is specified, --batch-size must be specified")
+        batchn, index = divmod(parsed_args.job_array_index, index_function.MAX_INDEX)
+        batch = slice(batch_size * batchn, batch_size * (batchn + 1), None)
+
+    R_tau = index_function(index)
+    joint_params = RadiusDuration({
+        RawATL09: DistanceFromLocation(
+            distance_km = R_tau.distance_km,
+            latitude = SITES[site]["lat"],
+            longitude = SITES[site]["lon"]
+        ),
+        RawCloudnet: Duration(duration = R_tau.tau)
+    })
+
+    # Convert argparse Namespace to Args dataclass
+    # Note: argparse uses dashes, but dataclass uses underscores
+    fpath_pickle = os.path.join(
+        parsed_args.pickle_dir, f"collocation_events_{site}.pkl" 
+    )
+    assert os.path.isfile(fpath_pickle)
+    assert os.path.isdir(parsed_args.output_dir)
+
+    return Args(
+        fpath_pickle = fpath_pickle,
+        site = site,
+        batched=batched,
+        batch = batch, 
+        dir_output = parsed_args.output_dir,
+        joint_params = joint_params
+    )
+
+
+
+def pair_acf_dataset_from_collocated_homogenised_data(coll_H):
+    """From two acf.ACF instances, join the [RawATL09 |  RawCloudnet] elements in a single xarray dataset.
+    A deep copy is made before returning so that no stray references to coll_H remain.
+    """
+    acf_atl09 = coll_H[RawATL09].data
+    acf_cloudnet = coll_H[RawCloudnet].data
+    return xr.Dataset(
+        data_vars = {
+            "acf_atl09": ((), acf_atl09),
+            "acf_cloudnet": ((), acf_cloudnet),
+        },
+    ).copy(deep=True)
+
+
+
+def make_safe(func: Callable[[Any],Any]) -> Callable[[Any],Any|None]:
+    """Make a provided function safe, by wrapping it in a try except statement.
+    If no exceptions are thrown, the return is [value, True].
+    If an exception is thrown, the return is [message, False].
+    """
+    def safe_func(*args, **kwargs):
+        try:
+            value = func(*args, **kwargs)
+            success = True
+        except Exception as e:
+            value = "\n".join((
+                f"make_safe({func.__qualname__})",
+                f"Thrown {type(e)}:",
+                str(e)
+            ))
+            success = False
+        return (value, success)
+    return safe_func
+
+# lambda to filter None from generator expressions
+filter_none = lambda generator: (v for v in generator if v is not None)
+
+
+
+def main(args: Args):
+    batch_collocation_events = CollocationEventList.from_file(args.fpath_pickle)[args.batch]
+    print(f"Loaded {args.fpath_pickle}, with {len(batch_collocation_events)} events in batch")
+
+    R_km = args.joint_params[RawATL09].distance_km
+    tau_s = int( args.joint_params[RawCloudnet].duration.total_seconds() )
+    fname_out = f"acfs-per-event_{args.site}_{R_km:3.3f}km_{tau_s:06}s"
+    if args.batched: fname_out += f"_batch_{args.batch.start}_{args.batch.stop}"
+    fname_out += ".nc"
+    fpath_out = os.path.join(
+        args.dir_output,
+        fname_out
+    )
+    print(f"{fpath_out=}")
+
+
+    # this time, I ask for no forgiveness
+    events_to_load = (
+        (
+            event, 
+            print("\n",f"LOADING",event)
+        )[0]
+        for event in batch_collocation_events
+    )
+
+    # reject events when the minimum ICESat-2 separation is gretaer than R_km:
+    N_reject_minimum_R = 0
+    events_to_load = filter_none((
+        event
+
+        if event[RawATL09].min_separation_km <= R_km
+        else (
+            None,
+            print(f"ABORTING due to min_separation_km={event[RawATL09].min_separation_km} > {R_km}"),
+            N_reject_minimum_R := N_reject_minimum_R + 1
+        )[0]
+
+        for event in events_to_load
+    ))
+
+    # load an event to a CollocatedRawData instance, and catch exceptions in loading
+    N_fail_on_load = 0
+    collocated_raw_data = filter_none((
+        raw_data[0] # make_safe value
+
+        if (
+            raw_data := make_safe(collocation_event.load_with_joint_parameters)(args.joint_params)
+        )[1] # make_safe success value
+
+        else (
+            None,
+            print(raw_data[0]), # make_safe message
+            print(f"ABORTING due to failed loading"),
+            N_fail_on_load:=N_fail_on_load+1,
+        )[0]
+
+        for collocation_event in events_to_load
+    ))
+
+    # filter for any of the elements of CollocatedRawData instances being None
+    N_fail_with_raw_data_None = 0
+    collocated_raw_data = filter_none((
+        raw_data
+
+        if not any(
+            value is None
+            for value in (raw_data).values()
+        )
+        else (
+            None,
+            print(f"ABORTING: None value found in collocated raw data, { {k: (type(v) is None) for k,v in raw_data.items()}= } (likely no data after subsetting)"),
+            N_fail_with_raw_data_None:=N_fail_with_raw_data_None+1
+        )[0]
+
+        for raw_data in collocated_raw_data
+    ))
+
+    # homogenise data, and catch exceptions
+    N_fail_homogenisation = 0
+    collocated_homogenised_data_and_n_profiles = filter_none((
+        (
+            collocated_h[0], # make_safe value
+            b:={k: rd.n_profiles for k, rd in raw_data_pair.items()},
+        )
+
+        if (
+            collocated_h := make_safe(raw_data_pair.homogenise_to)(acf.ACF)
+        )[1] # make_safe success value
+        else (
+            None,
+            print(type(collocated_h[0]), collocated_h[0]), # make_safe message
+            print(f"ABORTING: error thrown in homogenisation"),
+            N_fail_homogenisation:=N_fail_homogenisation+1,
+        )[0]
+
+        for raw_data_pair in collocated_raw_data
+    ))
+
+
+    # create acf data arrays from succesfully homogenised data
+    N_success = 0
+    n_profiles = {
+        RawATL09: list(),
+        RawCloudnet: list(),
+    }
+    paired_acfs = (
+        (
+            pair_acf_dataset_from_collocated_homogenised_data(coll_H),
+            N_success := N_success + 1,
+            a := n_profiles[RawATL09].append( num_profiles_dict[RawATL09] ),
+            b := n_profiles[RawCloudnet].append( num_profiles_dict[RawCloudnet] ),
+            print("Number of vertical profiles in event:", f"{num_profiles_dict}"),
+            print(f"SUCCESS: homogenised data loaded to xarray DataArray"),
+        )[0]
+
+        for coll_H, num_profiles_dict in collocated_homogenised_data_and_n_profiles
+    )
+
+    acfs_per_event = xr.concat(
+        paired_acfs,
+        dim="collocation_event"
+    )
+    acfs_per_event["n_profiles_atl09"] = ( ("collocation_event",) , n_profiles[RawATL09] )
+    acfs_per_event["n_profiles_cloudnet"] = ( ("collocation_event",) , n_profiles[RawCloudnet] )
+
+    print(f"SAVING TO {fpath_out=}")
+    acfs_per_event.to_netcdf(fpath_out)
+    print(f"SAVING SUCCESS")
+
+    # print additional information like the number of successes and failures
+    print("Additional information")
+    for label, value in zip(
+        ("loading", "none after laoding", "homogenissaton", "success", "total"),
+        (N_fail_on_load, N_fail_with_raw_data_None, N_fail_homogenisation, N_success, len(batch_collocation_events))
+    ):
+        print(f"{label:>25} | {value}")
+
+    print(n_profiles)
+    print(acf_per_event)
+
+    return
+
+
+    
+
+
+
+
+
+
+
+
+
+
