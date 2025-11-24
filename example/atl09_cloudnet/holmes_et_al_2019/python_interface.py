oopsy, that could've been bad. Corrected std computation to be std, not var

example/atl09_cloudnet/holmes_et_al_2019/python_interface.py
@@ -93,7 +93,7 @@ class MIEstimate:
     # computation is Eq. (8)/N from (10.1103/PhysRevE.100.022404). Communicationas with Holmes shows that this is the correct formulation of the formula, as 1. the chi2 distribution has an additional factor of 1/2 in the exponentiation, and 2. x~sigma_i is poorly defined, so a probability density based on a value a_i sigma_i^2 / B needs to be used, making use of the Jacobian |d(a_i sigma_i^2/B)/d(sigma_i^2)|
     a_i_div_N = (n_i - 1) / n_i
     k_i = n_i - 1
-    std_MI = (
+    std_MI = np.sqrt(
         np.sum(
             a_i_div_N * np.power(sigma_i, 2)
         )