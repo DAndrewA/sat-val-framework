Updated std computation to properly follow Holmes & Nemenman, with correct justification

example/atl09_cloudnet/holmes_et_al_2019/python_interface.py
@@ -25,7 +25,7 @@ LIB.MIxnyn.restype = None
 @dataclass(frozen=True, kw_only=True)
 class MIEstimate:
     MI: float
-    std_MI: float
+    std: float
     N: int
     n_splits: int
     sigma_i: list[int]
@@ -90,16 +90,19 @@ class MIEstimate:
 
     sigma_i = np.asarray(sigma_i)
     n_i = np.asarray(n_i)
+    # computation is Eq. (8)/N from (10.1103/PhysRevE.100.022404). Communicationas with Holmes shows that this is the correct formulation of the formula, as 1. the chi2 distribution has an additional factor of 1/2 in the exponentiation, and 2. x~sigma_i is poorly defined, so a probability density based on a value a_i sigma_i^2 / B needs to be used, making use of the Jacobian |d(a_i sigma_i^2/B)/d(sigma_i^2)|
+    a_i_div_N = (n_i - 1) / n_i
+    k_i = n_i - 1
     std_MI = (
         np.sum(
-            (n_i - 1) / n_i * np.power(sigma_i, 2)
+            a_i_div_N * np.power(sigma_i, 2)
         )
-        / np.sum( (n_i - 3) / 2 )
+        / np.sum(k_i)
     )
 
     return MIEstimate(
         MI = MI,
-        std_MI = std_MI,
+        std = std_MI,
         N = n_samples,
         n_splits = n_splits,
         sigma_i = list(sigma_i),