3a92624fd4d055d6fdab0d1ef5cfd6c974f71fa4
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-11-25 17:15:11 +0000

    finalised computation of figure 5 panels

--- example/figures/figure_5_case_study/figure.py
+++ example/figures/figure_5_case_study/figure.py
@@ -10,7 +10,7 @@ from common.colormaps import CMAP_N_events, CMAP_N_profiles, CMAP_MI
 from common.MI_plots import (
     DIR_MI,
     TEX_MI, TEX_R, TEX_tau, TEX_N_events, TEX_N_profiles,
-    PLOT_ARGS, 
+    PLOT_ARGS, FIGSIZE,
     K, 
     R_ticks, TAU_ticks, TAU_ticks_minor,
     R_slice,
@@ -30,7 +30,6 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm, Normalize
 
 SITE = "juelich"
-FIGSIZE = (12,3)
 
 
 
@@ -100,7 +99,32 @@ ds["pvalue_MImax"] = within_max_MI_pvalue(ds, "std")
 print("success")
 
 
-print("Generating plot")
-fig, axs = plot_results(ds)
-plt.savefig(fname_out:=f"{SITE}_k{K}_results.svg", format="svg", transparent=True, bbox_inches="tight")
+print("Generating plots")
+for da, TEX_da, CMAP, NORM_func, savename in (
+    (ds.N_events, TEX_N_events, CMAP_N_events, (lambda : LogNorm()), "n_events"),
+    (ds.N_profiles, TEX_N_profiles, CMAP_N_profiles, (lambda: LogNorm()), "n_profiles"),
+    (ds.MI, TEX_MI, CMAP_MI, (lambda : Normalize(vmin=ds.MI.min(), vmax=ds.MI.max())), "mutual_information"),
+):
+    fig, ax = plt.subplots(1,1, figsize=FIGSIZE, layout="constrained")
+    
+    plot_data_with_maxMI_and_significance(
+        da = da,
+        ds = ds,
+        TEX_da = TEX_da,
+        CMAP = CMAP,
+        NORM_func = NORM_func,
+        ax = ax
+    )
+
+    ax.set_title(None)
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlabel(TEX_R)
+    ax.set_ylabel(TEX_tau)
+    ax.set_xticks(*R_ticks)
+    ax.set_yticks(*TAU_ticks)
+    ax.set_yticks(*TAU_ticks_minor, minor=True)
+    ax.set_box_aspect(1)
+
+    plt.savefig(fname_out:=f"{SITE}_k{K}_{savename}.svg", format="svg", transparent=True, bbox_inches="tight")
 print(f"success, saved to {fname_out}")
