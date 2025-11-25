200d982124e53bbf1be0eb3762e8f127d3146038
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-11-25 10:47:01 +0000

    adapted slurm 05 to work for batch submission and compute MI with confidence

--- example/atl09_cloudnet/slurm/05-compute-MI/job-compute-MI.sh
+++ example/atl09_cloudnet/slurm/05-compute-MI/job-compute-MI.sh
@@ -6,12 +6,14 @@ script_name="atl09_cloudnet.scripts.compute_MI_with_confidence"
 dir_vcfs=$SCRATCH/vcfs_per_event
 out_dir=$SCRATCH/MI
 site=$1
+K=$2
 
 mkdir $out_dir
 
 mamba activate overpass_analysis_again
 
 echo "site = ${site}"
+echo "K = ${K}"
 echo "dir_vcfs = ${dir_vcfs}"
 echo "dir_out = ${dir_out}"
 echo "array-index = ${SLURM_ARRAY_TASK_ID}"
@@ -23,8 +25,8 @@ echo "array-index = ${SLURM_ARRAY_TASK_ID}"
         --site $site \
         --index-function "R_500km_tau_172800s" \
         --job-array-index $SLURM_ARRAY_TASK_ID \
-        -K 20 \
-        --n-bootstraps 500 \
-        --n-splits 5 \
-        --n-B-repeats 10 \
+        -K $K \
+        --n-bootstraps 100 \
+        --n-splits 10 \
+        --n-B-repeats 20 \
 )
