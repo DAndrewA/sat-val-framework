e3c8ea78b09ba6b67ca44cc651626e2379c2a293
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-11-28 18:53:27 +0000

    updated script to use errorbars and include additional legend, uploading final figure pdfs

--- example/figures/figure_9_10_11_MI_toy_model/figures.py
+++ example/figures/figure_9_10_11_MI_toy_model/figures.py
@@ -6,7 +6,7 @@ Script to produce 3 figures for the mutual information bounds appendix
 
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, '../../')
+sys.path.insert(1, "../../")
 from atl09_cloudnet.holmes_et_al_2019.python_interface import MIEstimate
 
 import scipy
@@ -21,7 +21,7 @@ K=10
 base = np.exp(1)
 RHOSTAR = np.sqrt(1 - np.power(base,-2))#np.sqrt(3)/2
 
-FIGSIZE = (6,4)
+FIGSIZE = (5,3)
 
 def gen_samples_mixture(n_samples: int, rhostar: float, mixture: float, rng: np.random.Generator | None = None):
     assert rhostar >= -1 and rhostar <= 1
@@ -110,7 +110,10 @@ def figure_1():
             MIE.std
             for MIE in MI_estimates_dependent
         ],
-        ls="none"
+        ls="none",
+        label="dependent",
+        marker="x",
+        capsize=2,
     )
 
     ax.errorbar(
@@ -123,11 +126,15 @@ def figure_1():
             MIE.std
             for MIE in MI_estimates_independent
         ],
-        ls="none"
+        ls="none",
+        label="independent",
+        marker="x",
+        capsize=2,
     )
 
     ax.set_xscale("log")
     ax.set_xlabel(r"$1 \slash N$")
+    ax.legend()
     ax.set_ylabel(r"$\hat{\text{I}}_\text{KSG}$ (nats)")
     
     plt.savefig("mi_as_function_of_n.svg", transparent=True, format="svg", bbox_inches="tight")
@@ -159,28 +166,40 @@ def figure_2():
 
     fig, ax = plt.subplots(1,1, figsize=FIGSIZE, layout="constrained")
 
-    ax.plot([0,1],[0,1], ls=(0,(10,2)), c="k", lw=1, label="theoretical bound")
+    ax.plot(
+        [0,1],[1,0], 
+        ls=(0,(10,2)), c="k", lw=1, 
+        label="theoretical bound",
+        path_effects = [patheffects.withTickedStroke(spacing=12, angle=-90, length=0.3)],
+    )
     ax.axhline(0, ls="--", c="k", lw=1, alpha=0.5)
 
-    ax.plot(
-        mixing_ratios,
-        [
+    ax.errorbar(
+        x=1 - mixing_ratios,
+        y=[
             MIE.MI
             for MIE in MI_estimates_mixtures
         ],
-        label=r"$\hat{\text{I}}_\text{KSG} ( X;Y \, | \, \kappa )$"
+        yerr=[
+            MIE.std
+            for MIE in MI_estimates_mixtures
+        ],
+        label=r"$\hat{\text{I}}_\text{KSG} ( X;Y \, | \, \kappa )$",
+        marker="x",
+        capsize=2,
     )
 
-    ax.set_xlim([1,0])
+    ax.set_xlim([0,1])
     ax.set_ylim([None, 1])
 
-    ax.set_xticks([0,1])
+    ax.set_xticks([0,1], [1,0])
     ax.set_yticks([0,1])
 
     ax.set_xlabel(r"$\kappa$")
     ax.set_ylabel(r"$\hat{\text{I}}_\text{KSG}$ (nats)")
 
     ax.legend()
+    ax.set_box_aspect(1)
 
     plt.savefig("mi_as_function_of_mixing_ratio.svg", format="svg", transparent=True, bbox_inches="tight")
 
@@ -258,16 +277,23 @@ def figure_3():
     #ax.axhline(1, ls="--", c="k", lw=1, alpha=0.5)
 
     for i, Nstar in enumerate(N_star):
-        ax.plot(
-            R_over_Rstar_values,
-            [
+        MIE_list = [ MIE for MIE in MI_by_R_by_sampling_density[i] ]
+
+        ax.errorbar(
+            x=R_over_Rstar_values,
+            y=[
                 MIE.MI
-                for MIE in MI_by_R_by_sampling_density[i]
+                for MIE in MIE_list
+            ],
+            yerr=[
+                MIE.std
+                for MIE in MIE_list
             ],
             label=f"$N^* = 10^{np.log10(Nstar):.0f}$",
             marker = markers[i],
             markevery = (i,4),
-            markersize=8
+            markersize=8,
+            capsize=2,
         )
 
     ax.set_xlabel(r"$R$")
