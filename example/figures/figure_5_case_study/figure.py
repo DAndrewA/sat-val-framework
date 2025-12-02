81e448f6181e2e1908cbc4260c9041a8d98bcd9b
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-12-02 15:15:42 +0000

    update figure 5 to generate legend

--- example/figures/figure_5_case_study/figure.py
+++ example/figures/figure_5_case_study/figure.py
@@ -27,6 +27,9 @@ from itertools import product
 from tqdm import tqdm
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
+from matplotlib.legend_handler import HandlerLine2D
 
 from matplotlib.colors import LogNorm, Normalize
 
@@ -79,6 +82,24 @@ def plot_results(ds) -> (plt.Figure, list[plt.Axes]):
     return fig, axs
 
 
+def legend_handles():
+    """Return legend handles for a vertical, horizontal dashed line, and hatching"""
+
+    def update_prop(handle, orig):
+        handle.update_from(orig)
+        x,y = handle.get_data()
+        handle.set_data([np.mean(x)]*2, [0, 2*y[0]])
+
+    handles = [
+        (vertical_line:=Line2D([0.5,0.5],[0,1], c="k", ls="--", lw=1, label=r"$\hat{R}$")),
+        Line2D([],[], c="k", ls="--", lw=1, label=r"$\hat{\tau}$"),
+        Patch(facecolor="none", hatch="xxx", label=r"possible $\hat{\boldsymbol{p}}$ candidate") 
+    ]
+    handler_map = {
+        vertical_line: HandlerLine2D(update_func=update_prop)
+    }
+    return handles, handler_map
+
 
 print("Loading dataset: ", end="")
 ds = get_MI_with_subsetting(site=SITE).load()
@@ -122,3 +143,9 @@ for da, TEX_da, CMAP, NORM_func, savename in (
 
     plt.savefig(fname_out:=f"{SITE}_k{K}_{savename}.svg", format="svg", transparent=True, bbox_inches="tight")
 print(f"success, saved to {fname_out}")
+
+fig, ax = plt.subplots(1,1, figsize=FIGSIZE, layout="constrained")
+handles, handler_map = legend_handles()
+legend = plt.legend(handles=handles, handler_map=handler_map, loc="upper left", handlelength=1, handleheight=1)
+ax.axis("off")
+plt.savefig("legend.svg", format="svg", transparent=True, bbox_inches="tight")
